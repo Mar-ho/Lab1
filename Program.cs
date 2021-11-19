46499d229fd8f0f40d770bc49add67c57d66ca51
Mar-ho <94583294+Mar-ho@users.noreply.github.com>
2021-11-19 17:45:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MarcinHoffmannLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz tekst");
+            string text = Console.ReadLine();
+            Console.WriteLine("Wpisz pierwszą liczbę");
+            decimal number = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Wpisz drugą liczbę");
+            decimal number2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"\nWpisałeś tekst:  { text }\n\nPierwsza liczba: { number }\nDruga liczba:    { number2 }");
+            Console.WriteLine($"\nWynik dodawania liczb:\t\t { number + number2 }");
+            Console.WriteLine($"Wynik odejmowania liczb:\t { number - number2 }");
+            Console.WriteLine($"Wynik mnożenia liczb:\t\t { number * number2 }");
+            Console.WriteLine($"Wynik dzielenia liczb:\t\t { number / number2 }");
+            Console.ReadKey();
+        }
+    }
+}
